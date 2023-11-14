2a800eb350db0ab1db74e95edadaea1450902ac7
Joshua Dogma <joshuadogma@Joshuas-MacBook-Pro.local>
2023-11-14 10:45:29 -0500

    added and rendered puppy list

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import {puppyList} from './data.js'
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppies)
 
   return (
     <>
-      <h1>Hello World</h1>
+      { 
+   puppies.map((puppy) => {
+     return <p key={puppy.id}>{puppy.name}</p>
+   })
+}
     </>
   )
 }
